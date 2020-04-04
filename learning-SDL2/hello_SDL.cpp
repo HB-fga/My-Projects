03f304c7281d8a336dd77418ee29f08630bc7e3e
Hugo Bezerra <hugoricardo.so.be@gmail.com>
2020-04-04 17:48:48 -0300

    color keying with SDL

--- learning-SDL2/hello_SDL.cpp
+++ learning-SDL2/hello_SDL.cpp
@@ -9,17 +9,6 @@
 const int SCREEN_WIDTH = 640;
 const int SCREEN_HEIGHT = 480;
 
-// constantes de cada textura para cada tecla
-enum KeyPressTextures
-{
-    KEY_PRESS_TEXTURE_DEFAULT,
-    KEY_PRESS_TEXTURE_UP,
-    KEY_PRESS_TEXTURE_DOWN,
-    KEY_PRESS_TEXTURE_LEFT,
-    KEY_PRESS_TEXTURE_RIGHT,
-    KEY_PRESS_TEXTURE_TOTAL
-};
-
 // Inicia a SDL e cria janela
 bool init();
 
@@ -37,6 +26,130 @@ SDL_Window* gWindow = NULL;
 
 // Renderizador da janela
 SDL_Renderer* gRenderer = NULL;
+
+// constantes de cada textura para cada tecla
+enum KeyPressTextures
+{
+    KEY_PRESS_TEXTURE_DEFAULT,
+    KEY_PRESS_TEXTURE_UP,
+    KEY_PRESS_TEXTURE_DOWN,
+    KEY_PRESS_TEXTURE_LEFT,
+    KEY_PRESS_TEXTURE_RIGHT,
+    KEY_PRESS_TEXTURE_TOTAL
+};
+
+// Classe para uma textura
+class LTexture
+{
+    private:
+        // A textura em si
+        SDL_Texture* mTexture;
+
+        // Dimensoes da imagem
+        int mWidth;
+        int mHeight;
+
+    public:
+        LTexture();
+        ~LTexture();
+
+        // Carrega imagem
+        bool loadFromFile( std::string path );
+
+        // Desaloca textura
+        void free();
+
+        // Renderiza textura no ponto dado
+        void render( int x, int y );
+
+        // Getter das dimensoes da imagem
+        int getWidth();
+        int getHeight();
+};
+
+LTexture::LTexture()
+{
+    // Inicializa
+    mTexture = NULL;
+    mWidth = 0;
+    mHeight = 0;
+}
+
+LTexture::~LTexture()
+{
+    // Desaloca
+    free();
+}
+
+bool LTexture::loadFromFile( std::string path )
+{
+    // Deleta textura preexistente
+    free();
+
+    // Textura final
+    SDL_Texture* newTexture = NULL;
+
+    // Carrega imagem
+    SDL_Surface* loadedSurface = IMG_Load( path.c_str() );
+    if( loadedSurface == NULL )
+    {
+        printf( "Unable to load image %s! SDL_image Error: %s\n", path.c_str(), IMG_GetError() );
+    }
+    else
+    {
+        // Color key da imagem
+        SDL_SetColorKey( loadedSurface, SDL_TRUE, SDL_MapRGB( loadedSurface->format, 0, 0xFF, 0xFF ) );
+        
+        // Cria textura a partir da surface
+        newTexture = SDL_CreateTextureFromSurface( gRenderer, loadedSurface );
+        if( newTexture == NULL )
+        {
+            printf( "Unable to create texture from %s! SDL Error: %s\n", path.c_str(), SDL_GetError() );
+        }
+        else
+        {
+            mWidth = loadedSurface->w;
+            mHeight = loadedSurface->h;
+        }
+
+        SDL_FreeSurface( loadedSurface );
+    }
+
+    mTexture = newTexture;
+    return mTexture != NULL;
+}
+
+void LTexture::free()
+{
+    if( mTexture != NULL )
+    {
+        SDL_DestroyTexture( mTexture );
+        mTexture = NULL;
+        mWidth = 0;
+        mHeight = 0;
+    }
+}
+
+void LTexture::render( int x, int y )
+{
+    // Define espaco de renderizacao e renderiza na tela
+    SDL_Rect renderQuad = { x, y, mWidth, mHeight };
+    SDL_RenderCopy( gRenderer, mTexture, NULL, &renderQuad );
+}
+
+int LTexture::getWidth()
+{
+    return mWidth;
+}
+
+int LTexture::getHeight()
+{
+    return mHeight;
+}
+
+// Texturas da cena
+LTexture gFooTexture;
+LTexture gBackgroundTexture;
 
 // Textura exibida atualmente
 SDL_Texture* gTexture = NULL;
@@ -90,7 +203,7 @@ bool init() {
     return success;
 }
 
-// Essa funcao se tornou obsoleta na aula 7 (Texture Loading and Rendering) junto com algumas outras linhas de codigo que foram apagadas
+// Aula 7 (Texture Loading and Rendering)
 // SDL_Surface* loadSurface( std::string path ) {
 
 //     // Imagem final e otimizada
@@ -150,6 +263,21 @@ bool loadMedia()
 
     bool success = true;
 
+    // Carrega carinha
+    if( !gFooTexture.loadFromFile( "guy.png" ) )
+    {
+        printf( "Failed to load guy texture image!\n" );
+        success = false;
+    }
+    
+    // Carrega fundo
+    if( !gBackgroundTexture.loadFromFile( "background.png" ) )
+    {
+        printf( "Failed to load background texture image!\n" );
+        success = false;
+    }
+
+
     // Carrega default
     gKeyPressTextures[ KEY_PRESS_TEXTURE_DEFAULT ] = loadTexture("press.png");
     if( gKeyPressTextures[ KEY_PRESS_TEXTURE_DEFAULT ] == NULL )
@@ -197,6 +325,8 @@ void close() {
     // Libera imagem carregada
     SDL_DestroyTexture(gTexture);
     gTexture = NULL;
+    gFooTexture.free();
+    gBackgroundTexture.free();
 
     // Destroi janela
     SDL_DestroyRenderer(gRenderer);
@@ -244,75 +374,76 @@ int main( int argc, char* args[] ) {
                     {
                         quit = true;
                     }
-                    // Usuario pressionou uma tecla
-                    else if( e.type == SDL_KEYDOWN )
-                    {
-                        // Seleciona a textura baseada na tecla
-                        switch( e.key.keysym.sym )
-                        {
-                            case SDLK_UP:
-                            gTexture = gKeyPressTextures[ KEY_PRESS_TEXTURE_UP ];
-                            break;
+                    
+                    // // aula 4 (Key presses)
+                    // // Usuario pressionou uma tecla
+                    // else if( e.type == SDL_KEYDOWN )
+                    // {
+                    //     // Seleciona a textura baseada na tecla
+                    //     switch( e.key.keysym.sym )
+                    //     {
+                    //         case SDLK_UP:
+                    //         gTexture = gKeyPressTextures[ KEY_PRESS_TEXTURE_UP ];
+                    //         break;
 
-                            case SDLK_DOWN:
-                            gTexture = gKeyPressTextures[ KEY_PRESS_TEXTURE_DOWN ];
-                            break;
+                    //         case SDLK_DOWN:
+                    //         gTexture = gKeyPressTextures[ KEY_PRESS_TEXTURE_DOWN ];
+                    //         break;
 
-                            case SDLK_LEFT:
-                            gTexture = gKeyPressTextures[ KEY_PRESS_TEXTURE_LEFT ];
-                            break;
+                    //         case SDLK_LEFT:
+                    //         gTexture = gKeyPressTextures[ KEY_PRESS_TEXTURE_LEFT ];
+                    //         break;
 
-                            case SDLK_RIGHT:
-                            gTexture = gKeyPressTextures[ KEY_PRESS_TEXTURE_RIGHT ];
-                            break;
+                    //         case SDLK_RIGHT:
+                    //         gTexture = gKeyPressTextures[ KEY_PRESS_TEXTURE_RIGHT ];
+                    //         break;
 
-                            default:
-                            gTexture = gKeyPressTextures[ KEY_PRESS_TEXTURE_DEFAULT ];
-                            // gTexture = NULL;
-                            break;
-                        }
-                    }
+                    //         default:
+                    //         gTexture = gKeyPressTextures[ KEY_PRESS_TEXTURE_DEFAULT ];
+                    //         // gTexture = NULL;
+                    //         break;
+                    //     }
+                    // }
                 }
 
-                // SDL_SetRenderDrawColor(gRenderer, 0xFF, 0xFF, 0xFF, 0xFF);
+                // aula 9 (The Viewport)
+                // // Limpa a tela
+                // SDL_RenderClear(gRenderer);
 
-                // Limpa a tela
-                SDL_RenderClear(gRenderer);
+                // // Criando um viewport superior esquerdo
+                // SDL_Rect topLeftViewport;
+                // topLeftViewport.x = 0;
+                // topLeftViewport.y = 0;
+                // topLeftViewport.w = SCREEN_WIDTH / 2;
+                // topLeftViewport.h = SCREEN_HEIGHT / 2;
+                // SDL_RenderSetViewport( gRenderer, &topLeftViewport );
 
-                // Criando um viewport superior esquerdo
-                SDL_Rect topLeftViewport;
-                topLeftViewport.x = 0;
-                topLeftViewport.y = 0;
-                topLeftViewport.w = SCREEN_WIDTH / 2;
-                topLeftViewport.h = SCREEN_HEIGHT / 2;
-                SDL_RenderSetViewport( gRenderer, &topLeftViewport );
+                // gTexture = gKeyPressTextures[ KEY_PRESS_TEXTURE_LEFT ];
+                // SDL_RenderCopy(gRenderer, gTexture, NULL, NULL);
 
-                gTexture = gKeyPressTextures[ KEY_PRESS_TEXTURE_LEFT ];
-                SDL_RenderCopy(gRenderer, gTexture, NULL, NULL);
+                // // Criando um viewport superior direito
+                // SDL_Rect topRightViewport;
+                // topRightViewport.x = SCREEN_WIDTH / 2;
+                // topRightViewport.y = 0;
+                // topRightViewport.w = SCREEN_WIDTH / 2;
+                // topRightViewport.h = SCREEN_HEIGHT / 2;
+                // SDL_RenderSetViewport( gRenderer, &topRightViewport );
 
-                // Criando um viewport superior direito
-                SDL_Rect topRightViewport;
-                topRightViewport.x = SCREEN_WIDTH / 2;
-                topRightViewport.y = 0;
-                topRightViewport.w = SCREEN_WIDTH / 2;
-                topRightViewport.h = SCREEN_HEIGHT / 2;
-                SDL_RenderSetViewport( gRenderer, &topRightViewport );
+                // gTexture = gKeyPressTextures[ KEY_PRESS_TEXTURE_RIGHT ];
+                // SDL_RenderCopy(gRenderer, gTexture, NULL, NULL);
 
-                gTexture = gKeyPressTextures[ KEY_PRESS_TEXTURE_RIGHT ];
-                SDL_RenderCopy(gRenderer, gTexture, NULL, NULL);
+                // // Criando um viewport inferior
+                // SDL_Rect bottomViewport;
+                // bottomViewport.x = 0;
+                // bottomViewport.y = SCREEN_HEIGHT / 2;
+                // bottomViewport.w = SCREEN_WIDTH;
+                // bottomViewport.h = SCREEN_HEIGHT / 2;
+                // SDL_RenderSetViewport(gRenderer, &bottomViewport);
 
-                // Criando um viewport inferior
-                SDL_Rect bottomViewport;
-                bottomViewport.x = 0;
-                bottomViewport.y = SCREEN_HEIGHT / 2;
-                bottomViewport.w = SCREEN_WIDTH;
-                bottomViewport.h = SCREEN_HEIGHT / 2;
-                SDL_RenderSetViewport(gRenderer, &bottomViewport);
+                // gTexture = gKeyPressTextures[ KEY_PRESS_TEXTURE_DOWN ];
+                // SDL_RenderCopy(gRenderer, gTexture, NULL, NULL);
 
-                gTexture = gKeyPressTextures[ KEY_PRESS_TEXTURE_DOWN ];
-                SDL_RenderCopy(gRenderer, gTexture, NULL, NULL);
-
-                // desenhando formas geometricas basicas
+                // aula 8 (Geometry Rendering)
                 // // Renderiza um retangulo vermelho
                 // SDL_Rect fillRect = {SCREEN_WIDTH / 4, SCREEN_HEIGHT / 4, SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2};
                 // SDL_SetRenderDrawColor(gRenderer, 0xFF, 0x00, 0x00, 0xFF);
@@ -333,6 +464,19 @@ int main( int argc, char* args[] ) {
                 // {
                 //     SDL_RenderDrawPoint( gRenderer, SCREEN_WIDTH / 2, i );
                 // }
+
+                // Limpa a tela
+                SDL_SetRenderDrawColor( gRenderer, 0xFF, 0xFF, 0xFF, 0xFF );
+                SDL_RenderClear( gRenderer );
+
+                //Render background texture to screen
+                gBackgroundTexture.render( 0, 0 );
+
+                //Render Foo' to the screen
+                gFooTexture.render( 240, 270 );
+
+                //Update screen
+                SDL_RenderPresent( gRenderer );
 
                 // Renderiza textura na tela
                 SDL_RenderCopy(gRenderer, gTexture, NULL, NULL);
